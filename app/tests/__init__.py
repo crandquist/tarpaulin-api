3d775960ba2c403547eb3f0943ea912db2103804
Cat Randquist <crandquist@gmail.com>
2025-06-05 10:56:55 -0700

    refactor: remove obsolete test files and fixtures

--- app/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Empty __init__.py file to make directory a Python package."""
--- app/tests/test_sanity.py
+++
@@ -1,8 +0,0 @@
-# tests/test_sanity.py
-
-def test_app_exists(client):
-    """
-    A trivial test to confirm that the Flask application is up.
-    """
-    resp = client.get("/")     # You might get 401 if "/" requires auth
-    assert resp.status_code in (200, 401, 404)
